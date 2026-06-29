28820e865d9787894dfda1af689f5af1e39778f1
j-m-upshaw <jalandmupshaw@gmail.com>
2026-06-29 12:19:42 -0500

    Updated the authentification session code

--- final_project/index.js
+++ final_project/index.js
@@ -11,7 +11,19 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    if (req.session.authorization)
+    {
+        let token = req.session.authorization['accessToken'];
+        //verify the JWT token generated for the session
+        jwt.verify(token, "access", (err, user) => {
+            if (!err) {
+                req.user = user;
+                next();//Moves on to the middleware
+            } else {
+                return res.status(403).json({ message: "User not authenticated" });
+            }
+        });
+    }
 });
  
 const PORT =5000;
